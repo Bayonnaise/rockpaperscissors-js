3f9687f6cd8fcf97da57bcb88c6a1cbd0ca118d9
David Bayon <dbayon82@googlemail.com>
2014-08-04 17:18:43 +0100

    Refactored into awesomeness

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -8,34 +8,29 @@ Player.prototype.picks = function(pick) {
 };
 
 Player.prototype._defeats = function(opponent) {
-	return Game.prototype.beats[this.pick].indexOf(opponent.pick) > -1;
+	return Object.keys(Game.prototype.PAIRS[this.pick]).indexOf(opponent.pick) > -1;
 };
 
-Player.prototype._victory_verb_over = function(opponent) {
-	var verb_index = Game.prototype.beats[this.pick].indexOf(opponent.pick) - 1;
-	return Game.prototype.beats[this.pick][verb_index];
-};
 
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
 }
 
-Game.prototype.beats = { 'rock' : ['blunts', 'scissors', 'crushes', 'lizard'],
-								'scissors' : ['cut', 'paper', 'decapitate', 'lizard'],
-								'paper' : ['covers', 'rock', 'disproves', 'Spock'],
-								'lizard' : ['eats', 'paper', 'poisons', 'Spock'],
-								'Spock' : ['smashes', 'scissors', 'vapourises', 'rock']
-							};
+Game.prototype.PAIRS = {
+	rock: { scissors: 'blunts', lizard: 'crushes' },
+	scissors: { paper: 'cut', lizard: 'decapitate' },
+	paper: { rock: 'covers', Spock: 'disproves' },
+	lizard: { paper: 'eats', Spock: 'poisons' },
+	Spock: { scissors: 'smashes', rock: 'vapourises' }
+};
 
 Game.prototype.winner = function() {
   if (this._isSamePick()) {
   	return null;
-  }
-  else if (this.player1._defeats(this.player2)) {
+  } else if (this.player1._defeats(this.player2)) {
   	return this.player1;
-  }
-  else {
+  } else {
   	return this.player2;
   }
 };
@@ -53,6 +48,6 @@ Game.prototype._isSamePick = function() {
 };
 
 Game.prototype.victoryMessage = function() {
-	var method = this.winner()._victory_verb_over(this.loser());
-	return this.winner().name + "'s " + this.winner().pick + " " + method + " " + this.loser().name + "'s " + this.loser().pick ;
+	var method = this.PAIRS[this.winner().pick][this.loser().pick];
+	return this.winner().name + "'s " + this.winner().pick + " " + method + " " + this.loser().name + "'s " + this.loser().pick;
 };
